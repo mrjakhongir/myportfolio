7e5ac2f7f4bae080efda4cc62a3c0013ad63b576
Jahongir <nusratovjahongir@gmail.com>
2023-08-10 10:53:24 +0500

    fix: fix toggle menu on mobile devices

--- frontend_portfolio/src/components/Navbar/Navbar.jsx
+++ frontend_portfolio/src/components/Navbar/Navbar.jsx
@@ -1,15 +1,18 @@
 import React, { useState } from "react";
 import "./navbar.scss";
 import { images } from "../../constants";
-import { HiMenuAlt4, HiX } from "react-icons/hi";
-import { motion } from "framer-motion";
+// import { HiMenuAlt4, HiX } from "react-icons/hi";
+// import { motion } from "framer-motion";
 
 const Navbar = () => {
-  const [toggle, setToggle] = useState(false);
+  const [toggleNav, setToggleNav] = useState(false);
+  function toggleNavigation() {
+    setToggleNav((prevState) => !prevState);
+  }
   return (
     <nav className="app__navbar">
       <div className="app__navbar-logo">
-        <a href="#">
+        <a href="#home">
           <img src={images.joxa} alt="logo" />
         </a>
       </div>
@@ -24,24 +27,23 @@ const Navbar = () => {
         )}
       </ul>
 
-      <div className="app__navbar-menu">
-        <HiMenuAlt4 onClick={() => setToggle(true)} />
+      <div
+        onClick={() => toggleNavigation()}
+        class={`hamburger ${toggleNav && "hamburger-toggled"}`}
+      >
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
 
-        {toggle && (
-          <motion.div
-            whileInView={{ x: [300, 0] }}
-            transition={{ duration: 0.85, ease: "easeOut" }}
-          >
-            <HiX onClick={() => setToggle(false)} />
-            <ul>
-              {["home", "about", "work", "skills", "contact"].map((item) => (
-                <li onClick={() => setToggle(false)} key={item}>
-                  <a href={`#${item}`}>{item}</a>
-                </li>
-              ))}
-            </ul>
-          </motion.div>
-        )}
+      <div className={`navigation ${toggleNav && "nav-open"}`}>
+        <ul>
+          {["home", "about", "work", "skills", "contact"].map((item) => (
+            <li onClick={() => toggleNavigation()} key={item}>
+              <a href={`#${item}`}>{item}</a>
+            </li>
+          ))}
+        </ul>
       </div>
     </nav>
   );
